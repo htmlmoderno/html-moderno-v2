f34a978701d343295b26e4111a315bcc294a5d4b
Alan Ktquez <ktquez@gmail.com>
2020-02-02 01:58:07 -0300

    Add webfontloader

--- src/.vuepress/theme/enhanceApp.js
+++ src/.vuepress/theme/enhanceApp.js
@@ -1,5 +1,14 @@
-import VueCompositionApi from '@vue/composition-api';
+import VueCompositionApi from '@vue/composition-api'
 
-export default ({ Vue, siteData }) => {
+import WebFontLoader from 'webfontloader'
+
+export default ({ Vue, isServer }) => {
   Vue.use(VueCompositionApi)
+  if (!isServer) {
+    WebFontLoader.load({
+      google: {
+        families: [process.env.VUE_APP_GOOGLE_FONT]
+      }
+    })
+  }
 }
