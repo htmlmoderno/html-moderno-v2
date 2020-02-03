e0185736bde6070903cbf805a8e3cdce428cd4b2
Alan Ktquez <ktquez@gmail.com>
2020-02-03 03:20:45 -0300

    Add ThePresentation component

---
+++ src/.vuepress/theme/components/layout/ThePresentation.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="presentation relative mt-24 sm:mt-40">
+    <div class="relative z-20">
+      <h1 class="font-medium w-full sm:w-3/4 lg:w-2/4">
+        <span class="uppercase block mb-2 text-accent">
+          {{ firstTitle }}
+        </span>
+        <span class="text-4xl">
+          {{ secondTitle }}<span class="text-accent">.</span>
+        </span>
+      </h1>
+    </div>
+    <div class="presentation__pattern absolute z-10 top-0 right-0 w-3/4">
+      <vp-icon
+        v-show="iconName"
+        :name="iconName"
+        class="presentation__icon absolute float-right hidden sm:block" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ThePresentation',
+    props: {
+      firstTitle: String,
+      secondTitle: String,
+      iconName: String
+    }
+  }
+</script>
+
+<style lang="scss">
+.presentation {
+  &__pattern {
+    top: 60%;
+    transform: translateY(-50%);
+    height: 140%;
+    background-image: linear-gradient(90deg, transparent 40%, #ECE6DC 30%);
+    background-size: 20px 5px;
+
+    @screen sm {
+      height: 180%;
+    }
+  }
+
+  &__icon {
+    font-size: 100px;
+    top: 50%;
+    right: 5%;
+    transform: translateY(-50%);
+  }
+}
+
+.mode-dark {
+  .presentation__pattern {
+    background-image: linear-gradient(90deg, transparent 40%, #4E5161 30%);
+  }
+}
+</style>
--- src/.vuepress/theme/views/Home.vue
+++ src/.vuepress/theme/views/Home.vue
@@ -1,14 +1,39 @@
 <template>
-  <div>
-    <h1>HOME</h1>
-    <Content></Content>
+  <div class="home">
+    <section>
+      <the-presentation
+        firstTitle="HTML Moderno"
+        secondTitle="Tornando o desenvolvimento web ainda mais interessante" />
+      <div class="w-full sm:w-1/4 text-center mt-20">
+        <span class="home__scroll-line relative inline-block bg-textLight dark:bg-textDark"></span>
+      </div>
+    </section>
+    <main>
+      AQUI VAI OS CARDS    
+    </main>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home'
+export default {
+  name: 'Home',
+  components: {
+    ThePresentation: () => import('@theme/components/layout/ThePresentation')
   }
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.home__scroll-line {
+  width: 2px;
+  height: 30vh;
+  &:before {
+    @apply absolute bottom-0 rounded-full;
+    background-color: inherit;
+    content: '';
+    width: 6px;
+    height: 6px;
+    left: -2px;
+  }
+}
+</style>
