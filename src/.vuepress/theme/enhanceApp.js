a9fa51f45ee1858f3b85fc8059ed8354e22ccf38
Alan Ktquez <ktquez@gmail.com>
2020-02-04 06:43:35 -0300

    Add vue-masonry-css

--- src/.vuepress/theme/enhanceApp.js
+++ src/.vuepress/theme/enhanceApp.js
@@ -1,5 +1,8 @@
 import VueCompositionApi from '@vue/composition-api'
+import VueMasonry from 'vue-masonry-css'
+
 
 export default ({ Vue }) => {
   Vue.use(VueCompositionApi)
+  Vue.use(VueMasonry)
 }
