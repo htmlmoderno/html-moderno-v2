3f96db17db16b24ade82c279fae429aa92569aa2
Alan Ktquez <ktquez@gmail.com>
2020-02-04 02:42:07 -0300

    Add in purgeCSS um pattern para a class "bg-cat..."

--- src/.vuepress/config/postcss.js
+++ src/.vuepress/config/postcss.js
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV !== 'development') {
   plugins.push(
     require('@fullhuman/postcss-purgecss')({
       whitelist: ['html', 'body', 'h1', 'h2', 'h3', 'mode-dark'],
+      whitelistPatterns: [/bg-cat-/],
       content: ['**/*.vue'],
       extractors: [
         {
