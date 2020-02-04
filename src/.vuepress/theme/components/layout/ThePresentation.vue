b284321776f19696a45fcf61a9e0aafa99d7650c
Alan Ktquez <ktquez@gmail.com>
2020-02-03 21:28:38 -0300

    Update style patterns

--- src/.vuepress/theme/components/layout/ThePresentation.vue
+++ src/.vuepress/theme/components/layout/ThePresentation.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="presentation relative mt-24 sm:mt-40">
     <div class="relative z-20">
-      <h1 class="font-medium w-full sm:w-3/4 lg:w-2/4">
+      <h1 class="font-medium w-full break-all sm:break-normal sm:w-3/4 lg:w-2/4">
         <span class="uppercase block mb-2 text-accent">
           {{ firstTitle }}
         </span>
@@ -10,7 +10,7 @@
         </span>
       </h1>
     </div>
-    <div class="presentation__pattern absolute z-10 top-0 right-0 w-3/4">
+    <div class="presentation__pattern patterns absolute z-10 top-0 right-0 w-3/4">
       <vp-icon
         v-show="iconName"
         :name="iconName"
@@ -36,8 +36,6 @@
     top: 60%;
     transform: translateY(-50%);
     height: 140%;
-    background-image: linear-gradient(90deg, transparent 40%, #ECE6DC 30%);
-    background-size: 20px 5px;
 
     @screen sm {
       height: 180%;
@@ -49,12 +47,6 @@
     top: 50%;
     right: 5%;
     transform: translateY(-50%);
-  }
-}
-
-.mode-dark {
-  .presentation__pattern {
-    background-image: linear-gradient(90deg, transparent 40%, #4E5161 30%);
   }
 }
 </style>
