9c477352d3144ac90c12fa649fb34ddb10416187
Alan Ktquez <ktquez@gmail.com>
2020-02-05 08:38:25 -0300

    Redirect to Home when not exists view

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -8,8 +8,8 @@ import { computed } from '@vue/composition-api'
 export default {
   name: 'Layout',
   setup (_, { root }) {
-    const view = computed(() => root.$frontmatter.view || 'Home')
-
+    const view = computed(() => root.$frontmatter.view)
+    if (!view.value) root.$router.push('/')
     return {
       view
     }
