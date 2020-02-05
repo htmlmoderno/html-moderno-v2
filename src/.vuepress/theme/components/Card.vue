a44edf1b7209bd60be56f6ceb75701c11a76b0b9
Alan Ktquez <ktquez@gmail.com>
2020-02-05 08:38:05 -0300

    Prepare data in CardPost

--- src/.vuepress/theme/components/Card.vue
+++ src/.vuepress/theme/components/Card.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="card n9m p-4">
-    <div class="w-full rounded-lg overflow-hidden">
+    <div
+      class="w-full rounded-lg overflow-hidden"
+      :class="{ 'mb-6': !data.image.src }"
+    >
       <router-link
         :to="data.to"
         class="hover:text-accent"
       >
         <img
+          v-if="data.image.src"
           class="w-full"
           :src="data.image.src"
           :alt="data.image.alt"
         >
+
+        <span
+          v-else
+          :class="`w-full h-3 block rounded-full bg-cat-${data.category}`"
+        />
       </router-link>
     </div>
-    <h2 class="mt-3 mb-10 text-lg font-medium">
+    <h2 class="mt-3 mb-10 text-lg font-medium tracking-normal leading-snug">
       <router-link
         :to="data.to"
         class="hover:text-accent"
@@ -30,9 +39,34 @@
 export default {
   name: 'Card',
   props: {
-    data: {
+    page: {
       type: Object,
       required: true
+    }
+  },
+  setup ({ page }) {
+    const slug = page.path.replace(/\//g, '')
+    const data = {
+      title: page.title,
+      category: '',
+      image: {},
+      timeago: '1 mês atrás',
+      to: page.path
+    }
+
+    if (page.frontmatter.categories.length) {
+      data.category = page.frontmatter.categories[0]
+    }
+
+    if (page.frontmatter.cover) {
+      data.image = {
+        src: `${page.frontmatter.cover[0].path}${slug}.${page.frontmatter.cover[0].extension}`,
+        alt: page.frontmatter.cover[0].alternativeText
+      }
+    }
+
+    return {
+      data
     }
   }
 }
