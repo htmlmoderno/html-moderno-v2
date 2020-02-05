3d04450a3d5fc56cfbac979f33487556800cd772
Alan Ktquez <ktquez@gmail.com>
2020-02-05 03:17:53 -0300

    Add router-link in Card component

--- src/.vuepress/theme/components/Card.vue
+++ src/.vuepress/theme/components/Card.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="card n9m p-4">
     <div class="w-full rounded-lg overflow-hidden">
-      <img
-        class="w-full"
-        :src="data.image.src"
-        :alt="data.image.alt"
+      <router-link
+        :to="data.to"
+        class="hover:text-accent"
       >
+        <img
+          class="w-full"
+          :src="data.image.src"
+          :alt="data.image.alt"
+        >
+      </router-link>
     </div>
     <h2 class="mt-3 mb-10 text-lg font-medium">
-      <nuxt-link
+      <router-link
         :to="data.to"
         class="hover:text-accent"
       >
         {{ data.title }}
-      </nuxt-link>
+      </router-link>
     </h2>
     <div class="pt-4 border-t border-solid border-light-200 dark:dark-200">
       <span class="text-xs">{{ data.timeago }}</span>
