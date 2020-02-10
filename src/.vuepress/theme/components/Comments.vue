efd7773fdbb7daaef6eb014eebf9e62d2ed73828
Alan Ktquez <ktquez@gmail.com>
2020-02-10 03:12:40 -0300

    Add Comment wrapper component

---
+++ src/.vuepress/theme/components/Comments.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="comments">
+    <h2 class="text-2xl mb-12 font-medium">
+      Comentários<span class="text-accent">.</span>
+    </h2>
+    <div class="mt-12">
+      <comment />
+    </div>
+  </div>
+</template>
+
+<script>
+import Comment from '@vuepress/plugin-blog/lib/client/components/Comment'
+export default {
+  name: 'Comments',
+  components: {
+    Comment
+  }
+}
+</script>
+
+<style lang="scss"></style>
