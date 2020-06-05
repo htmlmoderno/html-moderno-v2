cea87142461aa6784df14b56d6cdbf8d71c180be
Alan Ktquez <ktquez@gmail.com>
2020-06-04 23:49:52 -0300

    Update slugify

--- src/.vuepress/theme/utils/slugify.js
+++ src/.vuepress/theme/utils/slugify.js
@@ -1,49 +1,19 @@
-export default function slugify (text, separator) {
-  text = text.toString().toLowerCase().trim()
-
-  const sets = [
-    { to: 'a', from: '[ÀÁÂÃÄÅÆĀĂĄẠẢẤẦẨẪẬẮẰẲẴẶ]' },
-    { to: 'c', from: '[ÇĆĈČ]' },
-    { to: 'd', from: '[ÐĎĐÞ]' },
-    { to: 'e', from: '[ÈÉÊËĒĔĖĘĚẸẺẼẾỀỂỄỆ]' },
-    { to: 'g', from: '[ĜĞĢǴ]' },
-    { to: 'h', from: '[ĤḦ]' },
-    { to: 'i', from: '[ÌÍÎÏĨĪĮİỈỊ]' },
-    { to: 'j', from: '[Ĵ]' },
-    { to: 'ij', from: '[Ĳ]' },
-    { to: 'k', from: '[Ķ]' },
-    { to: 'l', from: '[ĹĻĽŁ]' },
-    { to: 'm', from: '[Ḿ]' },
-    { to: 'n', from: '[ÑŃŅŇ]' },
-    { to: 'o', from: '[ÒÓÔÕÖØŌŎŐỌỎỐỒỔỖỘỚỜỞỠỢǪǬƠ]' },
-    { to: 'oe', from: '[Œ]' },
-    { to: 'p', from: '[ṕ]' },
-    { to: 'r', from: '[ŔŖŘ]' },
-    { to: 's', from: '[ßŚŜŞŠ]' },
-    { to: 't', from: '[ŢŤ]' },
-    { to: 'u', from: '[ÙÚÛÜŨŪŬŮŰŲỤỦỨỪỬỮỰƯ]' },
-    { to: 'w', from: '[ẂŴẀẄ]' },
-    { to: 'x', from: '[ẍ]' },
-    { to: 'y', from: '[ÝŶŸỲỴỶỸ]' },
-    { to: 'z', from: '[ŹŻŽ]' },
-    { to: '-', from: '[·/_,:;\']' }
-  ]
-
-  sets.forEach(set => {
-    text = text.replace(new RegExp(set.from, 'gi'), set.to)
-  })
-
-  text = text.toString().toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
-    .replace(/\--+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
-
-  if ((typeof separator !== 'undefined') && (separator !== '-')) {
-    text = text.replace(/-/g, separator)
+export default function slugify (value, separator) {
+  const mapLetters = {
+    a: /(á|ã|â|à)/g,
+    e: /(é|ê)/g,
+    i: /(í)/g,
+    o: /(ó|ô|õ)/g,
+    u: /(ú)/g,
+    c: /(ç)/g,
+    n: /(ñ)/g
+  }
+  for (const letter in mapLetters) {
+    const rexp = mapLetters[letter]
+    value = value.toLowerCase() // :) Avoid include capital letters in regex
+      .replace(rexp, letter)
+      .replace(/(\s|\.)/g, separator) // Replace space by delimiter '-'
   }
 
-  return text
+  return value
 }
