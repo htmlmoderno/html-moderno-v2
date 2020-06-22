e1150b1026ac1d6ddaef769e3e339b31b064d2af
Alan Ktquez <ktquez@gmail.com>
2020-06-22 05:11:35 -0300

    Add IntersectionObserver component

---
+++ src/.vuepress/theme/components/IntersectionObserver.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    ref="ioWrapper"
+    class="h-10"
+    :class="{ 'h-auto': show }"
+  >
+    <slot :show="show" />
+  </div>
+</template>
+
+<script>
+import { onMounted, onDeactivated, ref } from '@vue/composition-api'
+
+export default {
+  name: 'IntersectionObserver',
+
+  props: {
+    threshold: {
+      type: Array,
+      default: () => [0, 0.2]
+    },
+    root: {
+      type: [HTMLElement, Object],
+      default: () => null
+    },
+    rootMargin: {
+      type: String,
+      default: () => '0px'
+    }
+  },
+
+  setup (props, { refs }) {
+    let observer = null
+    const show = ref(false)
+
+    onMounted(() => {
+      if ('IntersectionObserver' in window) {
+        observer = new IntersectionObserver(entries => {
+          if (entries[0].isIntersecting) {
+            show.value = true
+            observer.disconnect()
+          }
+        }, props)
+        observer.observe(refs.ioWrapper)
+      }
+    })
+
+    onDeactivated(() => {
+      observer.disconnect()
+    })
+
+    return {
+      show
+    }
+  }
+}
+</script>
