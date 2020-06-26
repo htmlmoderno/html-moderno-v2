cad76b7efa6d6b99481122822383e4964fa3003e
Alan Ktquez <ktquez@gmail.com>
2020-06-26 04:37:26 -0300

    fix: duplicate charset

--- src/.vuepress/config/head.js
+++ src/.vuepress/config/head.js
@@ -1,7 +1,6 @@
 
 module.exports = [
   ['meta', { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' }],
-  ['meta', { charset: 'utf-8' }],
   ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1' }],
   ['meta', { name: 'apple-mobile-web-app-title', content: 'HTML Moderno | Tornando o HTML ainda mais interessante.' }],
   ['meta', { name: 'theme-color', content: '#F4F1EC' }],
