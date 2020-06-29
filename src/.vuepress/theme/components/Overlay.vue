2af6cfcac87b01e390471b3519c8b5b65ece06bb
Alan Ktquez <ktquez@gmail.com>
2020-06-29 06:39:08 -0300

    Add Overlay component

---
+++ src/.vuepress/theme/components/Overlay.vue
@@ -0,0 +1,21 @@
+<template functional>
+  <div
+    v-show="props.show"
+    class="z-20 w-full h-full top-0 left-0 fixed bg-overlay"
+    aria-hidden="true"
+    v-on="listeners"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Overlay',
+
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
