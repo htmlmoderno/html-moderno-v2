acd71f67748e058696ef940669ea00c03bc2cabc
Alan Ktquez <ktquez@gmail.com>
2020-08-08 02:32:31 -0300

    Use local font

--- src/.vuepress/config/head.js
+++ src/.vuepress/config/head.js
@@ -58,7 +58,6 @@ module.exports = [
   ['link', { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' }],
   ['link', { rel: 'icon', type: 'image/png', href: '/favicon/favicon-32x32.png', sizes: '32x32' }],
   ['link', { rel: 'icon', type: 'image/png', href: '/favicon/favicon-16x16.png', sizes: '16x16' }],
-  ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Sintony:wght@400;700&display=swap' }],
 
   ['link', { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#ff5733' }],
   ['link', { id: 'favicon', rel: 'icon', href: '/favicon/favicon.svg' }],
